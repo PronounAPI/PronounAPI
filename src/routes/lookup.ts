e84db026a011b330f49ead5e667349dce634d9ab
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-03-18 15:20:30 +0000

    I love unfinished code

---
+++ src/routes/lookup.ts
@@ -0,0 +1,11 @@
+import { Route, RouteOptions } from '@tyman/modulo';
+
+@RouteOptions({
+    path: '/api/v1/lookup',
+    spec: {
+        
+    }
+})
+export default class LookupRoute extends Route {
+
+}

b1a9e4770e6eea0d9e5009db1488c099f765a968
TymanWasTaken <32660892+TymanWasTaken@users.noreply.github.com>
2022-03-19 05:22:03 +0000

    pain and suffering

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import { promises as fs } from 'fs'
 import path from 'path'
 import { Sequelize, Op } from 'sequelize';
 import * as config from './config'
+import { RouteManager } from '@tyman/modulo';
 
 interface PronounType {
     id: string;
@@ -78,4 +79,76 @@ interface UserResponse {
         ...p,
         creatorId: null
     })))
+
+    const routeManager = new RouteManager('routes', {
+        openapi: '3.0.0',
+        info: {
+            title: 'Pronoundb-custom',
+            description: 'A remake based on pronoundb, that maintains compatibility with the original',
+            license: {
+                name: 'NPOSL-3.0',
+                url: 'https://opensource.org/licenses/NPOSL-3.0'
+            },
+            version: '1.0.0'
+        },
+        components: {
+            schemas: {
+                Pronoun: {
+                    type: 'object',
+                    description: 'Details about a pronoun',
+                    properties: {
+                        id: {
+                            type: 'string',
+                            description: 'The ID of this pronoun. It is representative of the pronoun if it is a default pronoun, otherwise it is randomly generated.',
+                            example: 'itIts'
+                        
+                        },
+                        pronoundb: {
+                            type: 'string',
+                            nullable: true,
+                            description: 'The pronoundb ID for this pronoun, or null if there is no corrosponding pronoundb form.',
+                            example: 'ii'
+                        },
+                        pronoun: {
+                            type: 'string',
+                            description: 'The pronoun\'s nice name',
+                            example: 'it/its'
+                        },
+                        singular: {
+                            type: 'string',
+                            description: 'The singular form of this pronoun',
+                            example: 'it'
+                        },
+                        description: {
+                            type: 'string',
+                            description: 'The descriptive form of this pronoun',
+                            example: 'it'
+                        },
+                        ownership: {
+                            type: 'string',
+                            description: 'The posessive form of this pronoun',
+                            example: 'its'
+                        },
+                        subpronouns: {
+                            type: 'array',
+                            description: 'An array of other pronoun IDs that this pronoun contains (for pronouns like he/she or they/it)',
+                            items: {
+                                "$ref": "#/components/schemas/Pronoun"
+                            }
+                        }
+                    }
+                }
+            },
+            securitySchemes: {
+                Bearer: {
+                    type: 'http',
+                    scheme: 'bearer',
+                    bearerFormat: 'JWT'
+                }
+            }
+        },
+        paths: {}
+    })
+    await routeManager.loadRoutes()
+    await routeManager.serve(3000)
 })()
--- src/routes/lookup.ts
+++ src/routes/lookup.ts
@@ -1,11 +1,60 @@
 import { Route, RouteOptions } from '@tyman/modulo';
+import { Request, Response } from 'express';
 
 @RouteOptions({
     path: '/api/v1/lookup',
     spec: {
-        
+        get: {
+            description: 'Lookup a user from the database. Will fallback to pronoundb compatibility if the specified user is not found',
+            responses: {
+                '200': {
+                    description: 'The found user data',
+                    content: {
+                        'application/json': {
+                            schema: {
+                                type: 'object',
+                                description: 'The user\'s pronouns',
+                                properties: {
+                                    userId: {
+                                        type: 'string',
+                                        description: 'The ID of the fetched user'
+                                    },
+                                    platform: {
+                                        enum: [
+                                            'discord',
+                                            'facebook',
+                                            'github',
+                                            'twitch',
+                                            'twitter'
+                                        ],
+                                        description: 'The platform used to find this user'
+                                    },
+                                    preferredPronoun: {
+                                        $ref: '#/components/schemas/Pronoun'
+                                    },
+                                    extraPronouns: {
+                                        type: "array",
+                                        description: "An array of extra pronouns that this user has set",
+                                        items: {
+                                            $ref: "#/components/schemas/Pronoun"
+                                        }
+                                    },
+                                    pronoundbCompat: {
+                                        type: 'boolean',
+                                        default: 'Whether or not this pronoun was fetched from pronoundb because it was not stored in the normal database.',
+                                        example: true
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 })
 export default class LookupRoute extends Route {
-
+    async get(req: Request, res: Response) {
+        
+    }
 }
